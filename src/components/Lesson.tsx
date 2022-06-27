f59c0f9c7ea0b2df6721f208a3e78a84405d734b
KAIQUE ALMEIDA SILVA <43797086+kaiqueealm@users.noreply.github.com>
2022-06-27 12:03:34 -0300

    alteração

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 interface LessonProps{
   title: string;
@@ -11,6 +11,7 @@ interface LessonProps{
 }
 
 export function Lesson(props: LessonProps){
+  const { slug } = useParams<{ slug:string }>()
 
   const isLessonAvailable = isPast(props.availableAt)
   const availableDateFormatted = format(props.availableAt, "EEE' • 'd' de 'MMM' • 'k'h'mm",{
